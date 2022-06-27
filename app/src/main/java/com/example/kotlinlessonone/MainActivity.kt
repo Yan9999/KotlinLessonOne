16a4a26ba84b35dfd139a593f933c68487d18edd
Yan NOW <tumakaev.jan@yandex.ru>
2022-06-27 22:00:58 +0500

    Фраз - Тута- матута не выводится на экран не могу догнать

--- app/src/main/java/com/example/kotlinlessonone/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlessonone/MainActivity.kt
@@ -6,57 +6,22 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import com.example.kotlinlessonone.R
+import com.example.kotlinlessonone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bindinq: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val car:Car = Car("lada",4)
-
-        findViewById<Button>(R.id.button0).setOnClickListener(object : View.OnClickListener { //
-            // не понимаю как вынести это метод за MainActivity что бы было красиво
-            override fun onClick(v: View?) {
-                findViewById<TextView>(R.id.textView1).setText( "" + car.mileage  )
-                findViewById<TextView>(R.id.textView0).setText( car.name)
-
-            }
-        })
-
-        findViewById<Button>(R.id.button).setOnClickListener(object : View.OnClickListener { //
-            // не понимаю как вынести это метод за MainActivity что бы было красиво
-            override fun onClick(v: View?) {
-                findViewById<TextView>(R.id.textView).setText( "Приыет мир ")
-            }
-        })
-
-        val car2 = car.copy()
-
-        findViewById<TextView>(R.id.textView2).setText( "марка "+car2.name+" Пробег "+car2.mileage)
-
-
-
-        var index =1
-        var ex = " "
-        for(i in 1..10) {
-
-            ex = ex+index
-            findViewById<TextView>(R.id.textView3).setText(ex)
-            index++
-
-
-        }
-
-
-
-
-    }
+        bindinq = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindinq.root)
+        bindinq.textView.text = "Тута-Матута"
     }
 
+}
 
 
-
-data class Car( val name:String ="kia", var mileage:Int = 3 )
 
 
 
