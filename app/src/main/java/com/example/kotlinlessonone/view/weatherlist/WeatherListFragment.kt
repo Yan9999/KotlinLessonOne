29413d00d37d86444c21fd73d207bccee389ad00
Yan NOW <tumakaev.jan@yandex.ru>
2022-07-01 16:06:02 +0300

    3 урко работа с null and noNul ( смутно понял)

--- app/src/main/java/com/example/kotlinlessonone/view/weatherlist/WeatherListFragment.kt
+++ app/src/main/java/com/example/kotlinlessonone/view/weatherlist/WeatherListFragment.kt
@@ -12,15 +12,28 @@ import com.example.kotlinlessonone.databinding.FragmentWeatherListBinding
 import com.example.kotlinlessonone.viewmodel.AppState
 
 class WeatherListFragment:Fragment(){
+
     companion object{
         fun newInstance() = WeatherListFragment()
         }
-         lateinit var  binding :FragmentWeatherListBinding
+     private var  _binding :FragmentWeatherListBinding? = null
+    private val  binding :FragmentWeatherListBinding
+    get(){
+        return _binding!!
+    }
+
+    override fun onDestroy(){
+             super.onDestroy()
+             _binding = null
+         }
+
          lateinit var viewModel: WeatherListViewModel
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-      binding = FragmentWeatherListBinding.inflate(inflater)
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+      _binding = FragmentWeatherListBinding.inflate(inflater)
         return binding.root
     }
 
